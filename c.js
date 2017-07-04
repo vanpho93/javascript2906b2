7de3bf3c3aae644d88d3bcdbcb4b09acea698dbb
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-04 19:52:50 +0700

    xong arr

--- c.js
+++ c.js
@@ -5,6 +5,7 @@ const arr = ['NodeJS', 'ReactJS', 'Android', 'Javascript'];
 // arr.pop();
 // arr.shift();
 
-arr.splice(2, 0, 'PHP', 'iOS');
-
-console.log(arr);
+// arr.splice(2, 0, 'PHP', 'iOS');
+const str = 'NodeJS|||ReactJS|||Android|||Javascript';
+// console.log(arr.join('|||'));
+console.log(str.split('|||'));
