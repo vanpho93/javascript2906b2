325eecead84b0677ca5a84c7ec9e23eeca2edae7
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-04 19:49:31 +0700

    array

---
+++ b.js
@@ -0,0 +1,12 @@
+// const arr = new Array();
+const arr = [1, 3, 4, 5, 'KhoaPham'];
+
+// console.log(arr[2]);
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+arr.x = 1000;
+// for(e in arr) {
+//     console.log(e);
+// }
+console.log(arr['1'])
---
+++ c.js
@@ -0,0 +1,10 @@
+const arr = ['NodeJS', 'ReactJS', 'Android', 'Javascript'];
+
+// arr.unshift('Native');
+// arr.push('Native');
+// arr.pop();
+// arr.shift();
+
+arr.splice(2, 0, 'PHP', 'iOS');
+
+console.log(arr);
