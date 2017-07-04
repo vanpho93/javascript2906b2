ec8d0b9c8e185a73b23f42c8db58478603a5d7d7
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-04 21:21:56 +0700

    xong

---
+++ h.js
@@ -0,0 +1,15 @@
+const arr = [1, 3, 4, 2, 4, 5];
+
+arr.forEach(function(element) {
+    console.log(element);
+});
+
+function doForEach(mang, fn) {
+    for(let i = 0; i < mang.length; i++) {
+        fn(mang[i]);
+    }
+}
+
+doForEach(arr, function(e) {
+    console.log(e * e);
+});
