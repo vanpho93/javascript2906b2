9bdd6da77d9714f9c1764d46163fb2bd971766da
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-06 20:35:22 +0700

    filter

--- h.js
+++ h.js
@@ -1,15 +1,19 @@
 const arr = [1, 3, 4, 2, 4, 5];
 
-arr.forEach(function(element) {
-    console.log(element);
-});
+// arr.forEach(function(element) {
+//     console.log(element);
+// });
 
-function doForEach(mang, fn) {
-    for(let i = 0; i < mang.length; i++) {
-        fn(mang[i]);
-    }
+// function doForEach(mang, fn) {
+//     for(let i = 0; i < mang.length; i++) {
+//         fn(mang[i]);
+//     }
+// }
+
+// doForEach(arr, function(e) {
+//     console.log(e * e);
+// });
+
+function checkExist(anyArray, checkFunction) {
+
 }
-
-doForEach(arr, function(e) {
-    console.log(e * e);
-});
---
+++ i.js
@@ -0,0 +1,40 @@
+class Person {
+    constructor(name, height, age) {
+        this.name = name;
+        this.height = height;
+        this.age = age;
+    }
+}
+
+const arrPerson = [
+    new Person('Teo', 180, 15),
+    new Person('Ti', 170, 16),
+    new Person('Tun', 190, 14)
+];
+
+// arrPerson.forEach(e => console.log(' Xin chao, toi la ' + e.name));
+
+const isTallEnough = arrPerson.some(e => e.height > 185);
+
+// console.log(isTallEnough);
+
+// function checkExist(anyArray, checkFunction) {
+//     for (let i = 0; i < anyArray.length; i++) {
+//         if (checkFunction(anyArray[i])) return true;
+//     }
+//     return false;
+// }
+
+// console.log(checkExist(arrPerson, e => e.age > 10));
+
+function filter(anyArray, filterFunction) {
+    //return []
+    const arrResult = [];
+    anyArray.forEach(e => filterFunction(e) ? arrResult.push(e) : null);
+    return arrResult;
+}
+
+// console.log(filter(arrPerson, e => e.height > 180));
+
+const arr = arrPerson.filter(e => e.height > 175);
+console.log(arr);
